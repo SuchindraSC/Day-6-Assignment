a41e5fdb567c8552de90000e0ec5ad1d201646ef
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-08 13:10:08 +0530

    Coupon Number

---
+++ Coupon Number/Coupon Numbers/Coupon Numbers/Problem5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem5
+{
+    class Problem5
+    {
+        public static void StopWatch()
+        {
+            
+            Console.WriteLine("Press Any One Key To Start StopWatch ");
+            Console.ReadKey();
+
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+
+            Console.WriteLine("\nPress Any One Key To End Stop Watch ");
+            Console.ReadKey();
+            timer.Stop();
+
+            Console.WriteLine("\nStop at : {0} ",timer.Elapsed);
+        }
+
+        static void Main(String[] args)
+        {
+            StopWatch();
+        }
+    }
+}
---
+++ Coupon Number/Coupon Numbers/Coupon Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Coupon_Numbers
+{
+    class Program
+    {
+        static void CouponNumber(int num)
+        {
+            Random rand = new Random();
+            int Count = 0;
+            
+            while (true)
+            {
+                int RandomNumber = rand.Next(0, 101);
+                if (RandomNumber == num)
+                {
+                    Console.WriteLine("The Count is " + Count);
+                    break;
+                }
+                else
+                {
+                    Count++;
+                }
+            }
+           
+                
+            }
+           
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter A Number: ");
+            num = Convert.ToInt32(Console.ReadLine()); 
+            CouponNumber(num);
+        }
+    }
+}
