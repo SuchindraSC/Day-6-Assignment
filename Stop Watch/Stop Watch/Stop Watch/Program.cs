f9c143d5caea2db80bd809e84e9dea496d245ac4
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-08 13:13:17 +0530

    Stop Watch

---
+++ Stop Watch/Stop Watch/Stop Watch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Stop_Watch
+{
+    class Program
+    {
+        public static void StopWatch()
+        {
+
+            Console.WriteLine("Press Any One Key To Start StopWatch ");
+            Console.ReadKey();
+
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
+
+            Console.WriteLine("\nPress Any One Key To End Stop Watch ");
+            Console.ReadKey();
+            timer.Stop();
+
+            Console.WriteLine("\nStop at : {0} ", timer.Elapsed);
+        }
+
+        static void Main(String[] args)
+        {
+            StopWatch();
+        }
+    }
+}
