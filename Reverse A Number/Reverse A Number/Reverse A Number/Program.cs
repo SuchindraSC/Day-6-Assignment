75eedeeb110761a5e73f83a3fc86a7f38887e906
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-08 13:06:31 +0530

    Reverse A Number

---
+++ Reverse A Number/Reverse A Number/Reverse A Number/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Reverse_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number to reverse: ");
+            int number = int.Parse(Console.ReadLine());
+            int remainder, reverse = 0;
+            while (number > 0)
+            {
+                remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10;
+            }
+            Console.WriteLine("The Reverse Order is " + reverse);
+        }
+    }
+}
