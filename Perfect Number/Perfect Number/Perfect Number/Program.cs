64295bc8b523e1f1ca5b50e4a9ab736b0996cfc6
SuchindraSC <suchindrachitnis@gmail.com>
2021-08-08 13:00:51 +0530

    Perfect Number

---
+++ Perfect Number/Perfect Number/Perfect Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Perfect_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num, i, sum = 0;
+            Console.Write("Enter The Number To Check Whether The Number Is Perfect Number Or Not: ");
+            Num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= Num/2; i++)
+            {
+                if (Num % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine("\nThe Divisors Are {0}", i);
+                }
+            }
+            if (sum == Num)
+            {
+                Console.WriteLine("\n{0} Is A Perfect Number ", Num);
+            }
+            else
+            {
+                Console.WriteLine("\n{0} Is Not A Perfect Number ", Num);
+            }
+        }
+    }
+}
